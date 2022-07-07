91841cc13efe1a01955da4753aeef07f6e1917b7
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-07 18:43:43 -0400

    begin defining how constructing a network will work

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -2,6 +2,10 @@ pub struct Network {
     layers: Vec<Layer>,
 }
 
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
 struct Layer {
     neurons: Vec<Neuron>,
 }
@@ -12,6 +16,21 @@ struct Neuron {
 }
 
 impl Network {
+    pub fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
+    pub fn random(layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1);
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self { layers }
+    }
+
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
