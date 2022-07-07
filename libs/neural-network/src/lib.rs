a5434bc87e1f1709454328fba3638afb6e122adb
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-06 22:55:56 -0400

    implement network propagation in terms of layer propagation

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,5 +1,5 @@
 pub struct Network {
-    _layers: Vec<Layer>,
+    layers: Vec<Layer>,
 }
 
 struct Layer {
@@ -12,7 +12,15 @@ struct Neuron {
 }
 
 impl Network {
-    pub fn propagate(&self, _inputs: Vec<f32>) -> Vec<f32> {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+impl Layer {
+    fn propagate(&self, _inputs: Vec<f32>) -> Vec<f32> {
         todo!()
     }
 }
