41316323fd7bd4c994a26b89037aea3df5cd582e
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-06 22:45:11 -0400

    encode initial structure of neural network

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct Network {
+    _layers: Vec<Layer>,
+}
+
+struct Layer {
+    _neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    _bias: f32,
+    _weights: Vec<f32>,
+}
+
+impl Network {
+    pub fn propagate(&self, _inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
     }
 }
