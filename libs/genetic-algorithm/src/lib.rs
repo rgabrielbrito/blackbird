75ace5e1e02401220d18796f5704b87b1825f7b9
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-09 10:19:58 -0400

    define an individual trait possessing a fitness function

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,11 +1,18 @@
 pub struct GeneticAlgorithm;
 
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
 impl GeneticAlgorithm {
     pub fn new() -> Self {
         Self
     }
 
-    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
+    where
+        I: Individual,
+    {
         assert!(!population.is_empty());
 
         (0..population.len()).map(|_| todo!()).collect()
