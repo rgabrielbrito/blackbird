6883add97b109c7d3383c81c600c36d2a8b7e48e
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-09 12:23:29 -0400

    test uniform crossover

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -288,4 +288,20 @@ mod test {
             assert_eq!(genes[2], 2.0);
         }
     }
+
+    #[allow(clippy::float_cmp)]
+    #[test]
+    fn uniform_crossover() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let parent_a: Chromosome = (1..=100).map(|n| n as f32).collect();
+        let parent_b: Chromosome = (1..=100).map(|n| -n as f32).collect();
+
+        let child = UniformCrossover::default().crossover(&mut rng, &parent_a, &parent_b);
+
+        let diff_a = child.iter().zip(parent_a).filter(|(c, p)| *c != p).count();
+        let diff_b = child.iter().zip(parent_b).filter(|(c, p)| *c != p).count();
+
+        assert_eq!(diff_a, 49);
+        assert_eq!(diff_b, 51);
+    }
 }
