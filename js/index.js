4b7bfaa12994e974d56f3222023d23bbe5bbee6b
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-10 11:54:36 -0400

    serialize structs using serde (wasm does not support vectors yet)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 import * as sim from "blackbird-simulation-wasm";
 
 const simulation = new sim.Simulation();
+const world = simulation.world();
+console.log(world);
--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,12 +1,23 @@
 use blackbird_simulation as sim;
 use rand::prelude::*;
-use wasm_bindgen::prelude::*;
+use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Simulation {
     rng: ThreadRng,
     sim: sim::Simulation,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct World {
+    pub animals: Vec<Animal>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Animal {
+    pub x: f32,
+    pub y: f32,
 }
 
 #[wasm_bindgen]
@@ -18,10 +29,32 @@ impl Simulation {
 
         Self { rng, sim }
     }
+
+    pub fn world(&self) -> JsValue {
+        let world = World::from(self.sim.world());
+        JsValue::from_serde(&world).unwrap()
+    }
 }
 
 impl Default for Simulation {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl From<&sim::World> for World {
+    fn from(world: &sim::World) -> Self {
+        let animals = world.animals().iter().map(Animal::from).collect();
+
+        Self { animals }
+    }
+}
+
+impl From<&sim::Animal> for Animal {
+    fn from(animal: &sim::Animal) -> Self {
+        Self {
+            x: animal.position().x,
+            y: animal.position().y,
+        }
     }
 }
