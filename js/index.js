e7d4e95213aca9dc62e149e21051082875c605ab
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-10 11:14:28 -0400

    set up boilerplate for integrating the simulation crate with wasm

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 import * as sim from "blackbird-simulation-wasm";
 
-alert("Who's that dog " + sim.whos_that_dog() + "!");
+const simulation = new sim.Simulation();
--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,27 @@
+use blackbird_simulation as sim;
+use rand::prelude::*;
+use wasm_bindgen::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn whos_that_dog() -> String {
-    "Mister Peanutbutter".into()
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
+}
+
+impl Default for Simulation {
+    fn default() -> Self {
+        Self::new()
+    }
 }
