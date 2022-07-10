a4f5cf063c33ff93616752cecfd242dbae948bb4
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-10 12:21:50 -0400

    render birds as triangles using html canvas

--- js/index.js
+++ js/index.js
@@ -1,5 +1,38 @@
 import * as sim from "blackbird-simulation-wasm";
 
+CanvasRenderingContext2D.prototype.drawTriangle = function (x, y, size) {
+    this.beginPath();
+    this.moveTo(x, y);
+    this.lineTo(x + size, y + size);
+    this.lineTo(x - size, y + size);
+    this.lineTo(x, y);
+
+    this.fillStyle = 'rgb(0, 0, 0)';
+    this.fill();
+};
+
 const simulation = new sim.Simulation();
 const world = simulation.world();
-console.log(world);
+
+const viewport = document.getElementById('viewport');
+const viewportWidth = viewport.clientWidth;
+const viewportHeight = viewport.clientHeight;
+const viewportScale = window.devicePixelRatio || 1;
+
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
+const ctxt = viewport.getContext('2d');
+
+ctxt.fillStyle = 'rgb(0, 0, 0)';
+
+for (const animal of simulation.world().animals) {
+    ctxt.drawTriangle(
+        animal.x * viewportWidth,
+        animal.y * viewportHeight,
+        0.015 * viewportWidth,
+    );
+}
