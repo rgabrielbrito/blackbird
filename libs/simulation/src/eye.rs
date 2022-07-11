032ddcf058e7bca6b8a8a70e0f0c4197569efc38
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-11 10:37:43 -0400

    resolve errors regarding process_vision

--- libs/simulation/src/eye.rs
+++ libs/simulation/src/eye.rs
@@ -2,7 +2,7 @@ use crate::*;
 use std::f32::consts::*;
 
 const FOV_RANGE: f32 = 0.25;
-const FOV_RANGE: f32 = PI + FRAC_PI_4;
+const FOV_ANGLE: f32 = PI + FRAC_PI_4;
 const CELLS: usize = 9;
 
 #[derive(Debug)]
@@ -17,40 +17,49 @@ impl Eye {
         assert!(fov_range > 0.0);
         assert!(fov_angle > 0.0);
         assert!(cells > 0);
-        
-        Self { fov_range, fov_angle, cells }
+
+        Self {
+            fov_range,
+            fov_angle,
+            cells,
+        }
     }
 
-    pub fn cells (&self) -> usize {
+    pub fn cells(&self) -> usize {
         self.cells
     }
 
-    pub fn process_vision(&self, position: na::Point2<f32>, rotation: na::Point2<f32>, foods: &[f32]) -> Vec<f32> {
+    pub fn process_vision(
+        &self,
+        position: na::Point2<f32>,
+        rotation: na::Rotation2<f32>,
+        foods: &[Food],
+    ) -> Vec<f32> {
         let mut cells = vec![0.0; self.cells];
-        
+
         for food in foods {
             let vec = food.position - position;
             let dist = vec.norm();
-            
+
             if dist > self.fov_range {
                 continue;
             }
-            
+
             let angle = na::Rotation2::rotation_between(&na::Vector2::x(), &vec).angle();
             let angle = angle - rotation.angle();
             let angle = na::wrap(angle, -PI, PI);
-            
+
             if angle < -self.fov_angle / 2.0 || angle > self.fov_angle / 2.0 {
                 continue;
             }
-            
+
             let angle = angle + self.fov_angle / 2.0;
             let cell = angle / self.fov_angle * (self.cells as f32);
             let cell = (cell as usize).min(cells.len() - 1);
-            
+
             cells[cell] += (self.fov_range - dist) / self.fov_range;
         }
-        
+
         cells
     }
 }
