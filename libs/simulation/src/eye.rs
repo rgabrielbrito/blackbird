9ec08d867c2cf3a565b07d9e55651801f78f59a9
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-11 10:52:04 -0400

    test eyes by altering the fov ranges

--- libs/simulation/src/eye.rs
+++ libs/simulation/src/eye.rs
@@ -69,3 +69,82 @@ impl Default for Eye {
         Self::new(FOV_RANGE, FOV_ANGLE, CELLS)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_EYE_CELLS: usize = 13;
+
+    struct TestCase {
+        foods: Vec<Food>,
+        fov_range: f32,
+        fov_angle: f32,
+        x: f32,
+        y: f32,
+        rot: f32,
+        expected_vision: &'static str,
+    }
+
+    impl TestCase {
+        fn run(self) {
+            let eye = Eye::new(self.fov_range, self.fov_angle, TEST_EYE_CELLS);
+            let actual_vision = eye.process_vision(
+                na::Point2::new(self.x, self.y),
+                na::Rotation2::new(self.rot),
+                &self.foods,
+            );
+
+            let actual_vision: Vec<_> = actual_vision
+                .into_iter()
+                .map(|cell| {
+                    if cell >= 0.7 {
+                        "#"
+                    } else if cell >= 0.3 {
+                        "+"
+                    } else if cell > 0.0 {
+                        "."
+                    } else {
+                        ""
+                    }
+                })
+                .collect();
+
+            let actual_vision = actual_vision.join("");
+            assert_eq!(actual_vision, self.expected_vision);
+        }
+    }
+
+    fn food(x: f32, y: f32) -> Food {
+        Food {
+            position: na::Point2::new(x, y),
+        }
+    }
+
+    mod different_fov_ranges {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(1.0, "+")]
+        #[test_case(0.9, "+")]
+        #[test_case(0.8, "+")]
+        #[test_case(0.7, ".")]
+        #[test_case(0.6, ".")]
+        #[test_case(0.5, "")]
+        #[test_case(0.4, "")]
+        #[test_case(0.3, "")]
+        #[test_case(0.2, "")]
+        #[test_case(0.1, "")]
+        fn test(fov_range: f32, expected_vision: &'static str) {
+            TestCase {
+                foods: vec![food(1.0, 0.5)],
+                fov_angle: FRAC_PI_2,
+                x: 0.5,
+                y: 0.5,
+                rot: 0.0,
+                fov_range,
+                expected_vision,
+            }
+            .run()
+        }
+    }
+}
