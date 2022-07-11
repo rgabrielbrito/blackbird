b447f0db5e94243e93d3e9f5a599e3eb8aa03bbc
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-11 10:11:04 -0400

    define algorithm for detecting food in fov

--- libs/simulation/src/eye.rs
+++ libs/simulation/src/eye.rs
@@ -1,10 +1,62 @@
 use crate::*;
+use std::f32::consts::*;
+
+const FOV_RANGE: f32 = 0.25;
+const FOV_RANGE: f32 = PI + FRAC_PI_4;
+const CELLS: usize = 9;
 
 #[derive(Debug)]
-pub struct Eye;
+pub struct Eye {
+    fov_range: f32,
+    fov_angle: f32,
+    cells: usize,
+}
 
 impl Eye {
-    pub fn process_vision() -> Vec<f32> {
-        todo!()
+    fn new(fov_range: f32, fov_angle: f32, cells: usize) -> Self {
+        assert!(fov_range > 0.0);
+        assert!(fov_angle > 0.0);
+        assert!(cells > 0);
+        
+        Self { fov_range, fov_angle, cells }
+    }
+
+    pub fn cells (&self) -> usize {
+        self.cells
+    }
+
+    pub fn process_vision(&self, position: na::Point2<f32>, rotation: na::Point2<f32>, foods: &[f32]) -> Vec<f32> {
+        let mut cells = vec![0.0; self.cells];
+        
+        for food in foods {
+            let vec = food.position - position;
+            let dist = vec.norm();
+            
+            if dist > self.fov_range {
+                continue;
+            }
+            
+            let angle = na::Rotation2::rotation_between(&na::Vector2::x(), &vec).angle();
+            let angle = angle - rotation.angle();
+            let angle = na::wrap(angle, -PI, PI);
+            
+            if angle < -self.fov_angle / 2.0 || angle > self.fov_angle / 2.0 {
+                continue;
+            }
+            
+            let angle = angle + self.fov_angle / 2.0;
+            let cell = angle / self.fov_angle * (self.cells as f32);
+            let cell = (cell as usize).min(cells.len() - 1);
+            
+            cells[cell] += (self.fov_range - dist) / self.fov_range;
+        }
+        
+        cells
+    }
+}
+
+impl Default for Eye {
+    fn default() -> Self {
+        Self::new(FOV_RANGE, FOV_ANGLE, CELLS)
     }
 }
