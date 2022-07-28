f2970fd42fc441c68d4534ad1b3e5026418362a4
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-27 22:48:51 -0400

    implement a naive version of linear rank selection

--- libs/genetic-algorithm/src/selection/rank_selection.rs
+++ libs/genetic-algorithm/src/selection/rank_selection.rs
@@ -4,10 +4,32 @@ use crate::*;
 pub struct RankSelection;
 
 impl SelectionMethod for RankSelection {
-    fn select<'a, I>(&self, _rng: &mut dyn RngCore, _population: &'a [I]) -> &'a I
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
     where
         I: Individual,
     {
-        todo!()
+        let mut population: Vec<&I> = vec![population].into_iter().flatten().collect();
+
+        population.sort_by(|i, j| i.fitness().partial_cmp(&j.fitness()).unwrap());
+
+        let mut ranks: Vec<usize> = Vec::with_capacity(population.len());
+
+        for i in 0..population.len() {
+            ranks.push(i);
+        }
+
+        let total_fitness: f32 = population
+            .iter()
+            .map(|individual| individual.fitness())
+            .sum();
+
+        loop {
+            let individual_rank = ranks.choose(rng).expect("Population is empty");
+            let individual_share = *individual_rank as f32 / total_fitness;
+
+            if rng.gen_bool(individual_share as f64) {
+                return population[*individual_rank];
+            }
+        }
     }
 }
