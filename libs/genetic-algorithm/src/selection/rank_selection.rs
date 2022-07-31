8346abac290808d6cfaa217bc66e35e89e11d631
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-07-31 13:46:23 -0400

    resolve issue where rank selection would drop a member from population

--- libs/genetic-algorithm/src/selection/rank_selection.rs
+++ libs/genetic-algorithm/src/selection/rank_selection.rs
@@ -20,7 +20,7 @@ impl SelectionMethod for RankSelection {
         let mut ranks: Vec<usize> = Vec::with_capacity(population.len());
 
         for i in 0..population.len() {
-            ranks.push(i);
+            ranks.push(i + 1);
         }
 
         let total_fitness: f32 = population
@@ -33,8 +33,45 @@ impl SelectionMethod for RankSelection {
             let individual_share = *individual_rank as f32 / total_fitness;
 
             if rng.gen_bool(individual_share as f64) {
-                return population[*individual_rank];
+                return population[*individual_rank - 1];
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn roulette_wheel_selection() {
+        let method = RankSelection::default();
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(3.0),
+            TestIndividual::new(4.0),
+        ];
+
+        let actual_histogram = (0..1000)
+            .map(|_| method.select(&mut rng, &population))
+            .fold(BTreeMap::default(), |mut histogram, individual| {
+                *histogram.entry(individual.fitness() as i32).or_default() += 1;
+                histogram
+            });
+
+        let expected_histogram = maplit::btreemap! {
+            1 => 98,
+            2 => 202,
+            3 => 301,
+            4 => 399,
+        };
+
+        assert_eq!(actual_histogram, expected_histogram);
     }
 }
