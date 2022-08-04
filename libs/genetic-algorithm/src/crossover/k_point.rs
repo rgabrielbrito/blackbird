099ba4dde3dc92edba2646aeb824430d94aa7431
Roberto Gabriel Brito <ragabrielbrito@gmail.com>
2022-08-04 19:30:18 -0400

    add a comment describing how k-point crossover will work

--- libs/genetic-algorithm/src/crossover/k_point.rs
+++ libs/genetic-algorithm/src/crossover/k_point.rs
@@ -3,6 +3,11 @@ use crate::*;
 #[derive(Clone, Debug, Default)]
 pub struct KPointCrossover;
 
+// Rather than iterating through every point and choosing
+// either parent_a's value or parent_b's value with equal probability,
+// K point crossover picks K crossover points at random and swaps them.
+// For the purposes of this application, the K will most likely be fixed
+// rather than passing it as parameter to maintain the original api
 impl CrossoverMethod for KPointCrossover {
     fn crossover(
         &self,
